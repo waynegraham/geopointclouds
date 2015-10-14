29ad70ac18ca73e6403d01f4dc0749698e8f78a2
Wayne Graham <wayne.graham@virginia.edu>
2015-10-14 11:46:05 -0400

    Main setup before lunch

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,37 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+var pointSize = 1,
+    pointCountTarget = 1,
+    opacity = 1,
+    pointSizeType = Potree.PointSizeType.ADAPTIVE,
+    pointColorType = Potree.PointColorType.RGB,
+    pointShape = Potree.PointShape.SQUARE,
+    interpolate = false;
+
+var pointclousdPath = ""
+
+// var WGS84 = proj4.defs("WGS84");
+// var webMercator = proj4.def("EPSG:3857");
+
+// ol3 stuff
+var featureVector,
+    camFrustrumFeatureVector,
+    camFrustrum,
+    visibleBounds;
+
+// threejs stuff (and potree)
+var renderer,
+    scene,
+    sceneHUD,
+    referenceFrame,
+    camera,
+    cameraHUD,
+    skybox,
+    pointcloud,
+    clock = new THREE.Clock(),
+    showCoordinates = false,
+    mouse = {x: 0, y: 0},
+    elCoordinates = document.getElementById("lblCoordinates"),
+    showSkybox = true,
+    elRenderArea = document.getElementById("renderArea"),
+    progressBar = new Progressbar();
